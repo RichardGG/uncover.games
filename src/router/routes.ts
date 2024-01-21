80438fc9d752d53733dc4c8aebe434127fd806ca
Richard Gilbert <richard.gilbert@digistorm.com.au>
2024-01-21 19:27:47 +1000

    disable unused routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -13,17 +13,17 @@ const routes: RouteRecordRaw[] = [
     children: [{ name: 'website-callback', path: '', component: () => import('pages/website/Callback.vue') }],
   },
 
-  {
-    path: '/playnite/auth',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ name: 'playnite-auth', path: '', component: () => import('pages/playnite/Auth.vue') }],
-  },
+  // {
+  //   path: '/playnite/auth',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [{ name: 'playnite-auth', path: '', component: () => import('pages/playnite/Auth.vue') }],
+  // },
 
-  {
-    path: '/playnite/callback',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ name: 'playnite-callback', path: '', component: () => import('pages/playnite/Callback.vue') }],
-  },
+  // {
+  //   path: '/playnite/callback',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [{ name: 'playnite-callback', path: '', component: () => import('pages/playnite/Callback.vue') }],
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
