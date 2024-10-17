9806654ae8158c73211cf800531909d92ddb0403
Richard Gilbert <richard.gilbert@digistorm.com.au>
2024-10-18 07:34:12 +1000

    Responsive grid view, better sizing

--- src/components/LibraryViews/GridView.vue
+++ src/components/LibraryViews/GridView.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <q-virtual-scroll
-      v-slot="{ item, index: rowIndex }"
+      v-slot="{ item: row }"
       style="max-height: calc(100vh - 100px);"
       :items="gameRows"
-      separator
-      :virtual-scroll-item-size="200"
+      :virtual-scroll-item-size="rowHeight"
     >
       <q-item
-        :key="rowIndex"
+        :key="getRowIndex(row)"
         dense
+        style="padding-left: 0; padding-right: 0;"
       >
         <q-item-section>
           <div class="flex">
-            <div v-for="(game, colIndex) in item" :key="colIndex">
+            <div v-for="(game, colIndex) in row" :key="colIndex">
               <Cover :title="game.Name" :file-name="game.CoverImage" :width="gameWidth - 10" style="margin: 0 5px" />
             </div>
           </div>
@@ -39,10 +39,11 @@ export default defineComponent({
     }
   },
   setup (props) {
-    const rowSize = ref(5)
     const windowSize = ref(1000)
+    const rowSize = computed(() => Math.floor(windowSize.value / 130))
+    const rowHeight = computed(() => (windowSize.value / rowSize.value) * 2)
     const gameRows = computed(() => chunk(props.games, rowSize.value))
-    const gameWidth = computed(() => (windowSize.value - 50) / rowSize.value)
+    const gameWidth = computed(() => (windowSize.value - 20) / rowSize.value)
     const onResize = () => {
       windowSize.value = window.innerWidth
     }
@@ -56,7 +57,11 @@ export default defineComponent({
       window.removeEventListener('resize', onResize)
     })
 
-    return { gameRows, gameWidth, windowSize }
+    const getRowIndex = (row: Game[]) => {
+      return row.reduce<string>((prev: string, current: Game): string => `${prev}:${current.Id}`, '')
+    }
+
+    return { gameRows, gameWidth, windowSize, getRowIndex, rowHeight }
   }
 })
 </script>
