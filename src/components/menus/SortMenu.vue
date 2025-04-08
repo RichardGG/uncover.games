588eb202b5011b7631cb9b2839ade67bbee5df5d
Richard Gilbert <richard.gilbert@digistorm.com.au>
2025-04-09 08:18:17 +1000

    Improved sort menu

--- src/components/menus/SortMenu.vue
+++ src/components/menus/SortMenu.vue
@@ -1,41 +1,123 @@
 <script setup lang="ts">
-import { Popover, SelectButton } from 'primevue'
-import { useTemplateRef } from 'vue'
-import { SortOrder, sortOrderTranslations, SortOrderDirection } from '@/types/SortTypes'
+import {
+  PhTextT,
+  PhPackage,
+  PhStorefront,
+  PhFolders,
+  PhClock,
+  PhCar,
+  PhCalendar,
+  PhCode,
+  PhCurrencyDollar,
+  PhTag,
+  PhCards,
+  PhPerson,
+  PhAsterisk,
+  PhGlobe,
+  PhShoppingBag,
+  PhPlusCircle,
+  PhCheck,
+  PhThumbsUp,
+  PhStamp,
+  PhSmiley,
+  PhCalendarPlus,
+  PhPencil,
+  PhDownload,
+  PhEyeSlash,
+  PhStar,
+  PhHardDrives,
+  PhGameController,
+  PhBoxArrowDown,
+  PhJoystick,
+} from '@phosphor-icons/vue'
+import { Menu, SelectButton } from 'primevue'
+import { useTemplateRef, computed } from 'vue'
+import { SortOrder, SortOrderDirection, sortOrderTranslations } from '@/types/SortTypes'
 import { useAppStore } from '@/stores/appStore'
 
 const appStore = useAppStore()
 
 const menu = useTemplateRef('menu')
 
+const sortKeys = Object.keys(SortOrder).filter(k => isNaN(Number(k))) as (keyof typeof SortOrder)[]
+const sortOptions = computed(() => sortKeys.map((key) => ({
+  label: sortOrderTranslations[SortOrder[key]],
+  command: () => appStore.currentFilter.SortingOrder = SortOrder[key],
+  value: SortOrder[key],
+  class: appStore.currentFilter.SortingOrder === SortOrder[key] 
+    ? 'bg-(--p-menu-item-focus-background)/50 rounded font-medium text-primary' 
+    : ''
+})))
+
+const scrollToTop = () => {
+  if (menu.value) {
+    // TODO this doesn't work
+    // console.log('open menu', menu.value.$el)
+    // menu.value.$el.querySelector('.overflow-y-scroll')[0].scrollTop = 0
+  }
+}
+
 defineExpose({ menu })
 </script>
 <template>
-  <Popover ref="menu">
-    <SelectButton
-      v-model="appStore.currentFilter.SortingOrderDirection"
-      option-label="name"
-      option-value="value"
-      :options="[
-        {
-          value: SortOrderDirection.Ascending,
-          name: 'Ascending', 
-        },
-        {
-          value: SortOrderDirection.Descending,
-          name: 'Descending'
-        }
-      ]"
-    />
-    <ul class="list-none p-0 m-0 flex flex-col w-52">
-      <li
-        v-for="order in Object.keys(SortOrder).filter(k => isNaN(Number(k))) as (keyof typeof SortOrder)[]"
-        :key="order"
-        class="flex items-center gap-2 px-2 rounded-md h-8 hover:bg-highlight-emphasis cursor-pointer"
-        @click="appStore.currentFilter.SortingOrder = SortOrder[order]"
-      >
-        <span class="font-medium">{{ SortOrder[order] }} - {{ sortOrderTranslations[SortOrder[order]] }}</span>
-      </li>
-    </ul>
-  </Popover>
+  <Menu
+    ref="menu"
+    :model="sortOptions"
+    :popup="true"
+    class="max-h-[50vh] overflow-y-scroll"
+    @show="scrollToTop"
+  >
+    <template #start>
+      <SelectButton
+        v-model="appStore.currentFilter.SortingOrderDirection"
+        option-label="name"
+        option-value="value"
+        :options="[
+          {
+            value: SortOrderDirection.Ascending,
+            name: 'Ascending', 
+          },
+          {
+            value: SortOrderDirection.Descending,
+            name: 'Descending'
+          }
+        ]"
+      />
+    </template>
+    <template #itemicon="slotProps">
+      <div :class="appStore.currentFilter.SortingOrder === slotProps.item.value ? 'text-primary' : 'text-surface'">
+        <PhTextT v-if="slotProps.item.value === SortOrder.Name" />
+        <PhPackage v-if="slotProps.item.value === SortOrder.Platforms" />
+        <PhStorefront v-if="slotProps.item.value === SortOrder.Library" />
+        <PhFolders v-if="slotProps.item.value === SortOrder.Categories" />
+        <PhClock v-if="slotProps.item.value === SortOrder.LastActivity" />
+        <PhCar v-if="slotProps.item.value === SortOrder.Genres" />
+        <PhCalendar v-if="slotProps.item.value === SortOrder.ReleaseDate" />
+        <PhCode v-if="slotProps.item.value === SortOrder.Developers" />
+        <PhCurrencyDollar v-if="slotProps.item.value === SortOrder.Publishers" />
+        <PhTag v-if="slotProps.item.value === SortOrder.Tags" />
+        <PhCards v-if="slotProps.item.value === SortOrder.Series" />
+        <PhPerson v-if="slotProps.item.value === SortOrder.AgeRatings" />
+        <PhAsterisk v-if="slotProps.item.value === SortOrder.Version" />
+        <PhGlobe v-if="slotProps.item.value === SortOrder.Regions" />
+        <PhShoppingBag v-if="slotProps.item.value === SortOrder.Source" />
+        <PhPlusCircle v-if="slotProps.item.value === SortOrder.PlayCount" />
+        <PhClock v-if="slotProps.item.value === SortOrder.Playtime" />
+        <PhCheck v-if="slotProps.item.value === SortOrder.CompletionStatus" />
+        <PhThumbsUp v-if="slotProps.item.value === SortOrder.UserScore" />
+        <PhStamp v-if="slotProps.item.value === SortOrder.CriticScore" />
+        <PhSmiley v-if="slotProps.item.value === SortOrder.CommunityScore" />
+        <PhCalendarPlus v-if="slotProps.item.value === SortOrder.Added" />
+        <PhPencil v-if="slotProps.item.value === SortOrder.Modified" />
+        <PhDownload v-if="slotProps.item.value === SortOrder.IsInstalled" />
+        <PhEyeSlash v-if="slotProps.item.value === SortOrder.Hidden" />
+        <PhStar v-if="slotProps.item.value === SortOrder.Favorite" />
+        <PhHardDrives v-if="slotProps.item.value === SortOrder.InstallDirectory" />
+        <PhGameController v-if="slotProps.item.value === SortOrder.Features" />
+        <PhBoxArrowDown v-if="slotProps.item.value === SortOrder.InstallSize" />
+        <PhPencil v-if="slotProps.item.value === SortOrder.RecentActivity" />
+        <PhJoystick v-if="slotProps.item.value === SortOrder.RomList" />
+      </div>
+    </template>
+  </Menu>
 </template>
